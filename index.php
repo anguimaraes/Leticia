49db3d306090c9e4237a3d08a760768acc03c00a
anguimaraes <andregtst@gmail.com>
2022-08-12 09:59:28 -0300

    commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<html>
+
+<head>
+    <title>Amor da minha vida</title>
+    <!-- Importando o css do bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DynaPuff&display=swap" rel="stylesheet">
+    <link rel="icon" 
+      type="image/jpg" 
+      href="heart256_icon-icons.com_76061.ico" />
+    <style>
+        body {
+            background: #FFFFFF url("papel-de-parede-adesivo-coracoes-coloridos-n04075-499313.webp");
+        }
+
+        .titulo {
+            margin-left: 40rem;
+            margin-top: 10rem;
+            color: red;
+        }
+
+        .center {
+            margin-left: 45rem;
+            margin-top: 15rem;
+        }
+
+        .vermelho {
+            background-color: #FFC0CB;
+        }
+
+        .fonte {
+            font-family: 'DynaPuff', cursive;
+        }
+    </style>
+
+
+</head>
+
+<body>
+    <h1 class="titulo">Quando a sua esposa pensa:</h1>
+    <div class="center">
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#MyModal">Será que ele pensa em mim no trabalho?</button>
+    </div>
+
+    <div id="MyModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header vermelho">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title fonte">André e Leticia</h4>
+                </div>
+                <div class="modal-body">
+                    <p class="fonte">Penso o tempo todo meu amor. Estou longe, mas perto do seu coração.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-rounded btn-danger" data-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+<!-- Importando o jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+<!-- Importando o js do bootstrap -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</html>
